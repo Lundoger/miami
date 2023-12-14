9565a3304343628496eb342f31d2593c9e725228
Богдан <sysoevbogdan82@gmail.com>
2023-12-14 14:03:54 +0200

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,11 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+
+const appHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight();
